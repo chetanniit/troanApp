9bafab383ffe9c9e7cc54ef22a00ffdc49b8f482
Chetan <chetan.niit@gmail.com>
2025-08-09 15:06:54 +0530

    ok

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
+	
+server: {
+    allowedHosts: ['devserver-master--tronapp.netlify.app'],
+  },
   plugins: [
     nodePolyfills(),
     react()
